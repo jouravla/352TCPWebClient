50ce38866787020f072bfad7a26459732f4e5601
jouravla <jouravla@dickinson.edu>
2017-03-01 13:41:38 -0500

    Implemented working version of client (Part 1)

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,4 +1,41 @@
+import java.io.*;
+import java.net.*;
 
-public class TCPClient {
+/**
+ * 
+ * 
+ * CTRL + C will terminate the program
+ * 
+ * @author - Sasha Jouravlev
+ */
+class TCPClient {
+	
+	public static void main(String args[]) throws Exception{
+		//Read in the command line arguments
+		String hostname = args[0];
+		int port = Integer.parseInt(args[1]);
+		String resource = "";
+		
+		if(args.length == 3){
+			resource = args[2];
+		}
 
+		//Establish a TCP connection with the mail server.
+		Socket clientSocket = new Socket(hostname, port);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+		
+		//Send the HTTP Request
+		outToServer.writeBytes("GET " + resource + " HTTP/1.1\n"+ "Host: " + hostname + "\n\n");
+		
+		String upNext;
+		while((upNext=inFromServer.readLine())!=null){
+			System.out.println(upNext);
+		}
+		
+		inFromServer.close();
+		clientSocket.close();
+		System.exit(1);
+	}
 }
